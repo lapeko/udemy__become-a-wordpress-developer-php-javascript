72b9de99809385467d3a3d62ddc252ed8743bdba
lapeko <lapeko88@gmail.com>
2024-11-30 20:14:55 +0100

    50. Reduce Duplication - "get_template_part()"

--- www/html/wp-content/themes/fictional-university-theme/archive-event.php
+++ www/html/wp-content/themes/fictional-university-theme/archive-event.php
@@ -8,25 +8,11 @@
 ?>
 	<div class="container container--narrow page-section">
 		<?php
-            while(have_posts()) {
-            the_post();
-        ?>
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                    <?php $eventDate = new DateTime(get_field('event_date')) ?>
-                    <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-                    <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-                </div>
-            </div>
-        <?php
-		    }
+            while(have_posts()): the_post();
+                get_template_part('template-parts/content-event');
+		    endwhile;
             echo paginate_links();
         ?>
-
         <hr class="section-break" />
         <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events archive.</p>
 	</div>
--- www/html/wp-content/themes/fictional-university-theme/page-past-events.php
+++ www/html/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -23,19 +23,7 @@
 				)
 			));
 			while ($past_events->have_posts()): $past_events->the_post();
-		?>
-				<div class="event-summary">
-					<a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-						<?php $eventDate = new DateTime(get_field('event_date')) ?>
-						<span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-						<span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-					</a>
-					<div class="event-summary__content">
-						<h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-						<p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-					</div>
-				</div>
-		<?php
+		        get_template_part('template-parts/content-event');
 			endwhile;
             wp_reset_postdata();
 			echo paginate_links();
