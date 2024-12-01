e74b5e313a045a6c3feb23ecb3571824681fd4d9
lapeko <lapeko88@gmail.com>
2024-12-01 16:04:55 +0100

    63. Keyboard Events in JavaScript

--- www/html/wp-content/themes/fictional-university-theme/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'befd9d343593d27a6835');
--- www/html/wp-content/themes/fictional-university-theme/src/modules/search.js
+++ www/html/wp-content/themes/fictional-university-theme/src/modules/search.js
@@ -3,9 +3,28 @@ const closeSearchBtn = document.querySelector('.search-overlay__close');
 const searchOverlay = document.querySelector('.search-overlay');
 
 const activeOverlayClass = 'search-overlay--active';
+const bodyNoScrollClass = 'body-no-scroll';
 
-const openOverlay = () => searchOverlay.classList.add(activeOverlayClass);
-const closeOverlay = () => searchOverlay.classList.remove(activeOverlayClass);
+let overlayOpened = false;
+
+const openOverlay = () => {
+    if (overlayOpened) return;
+    searchOverlay.classList.add(activeOverlayClass);
+    document.body.classList.add(bodyNoScrollClass);
+    overlayOpened = true;
+}
+const closeOverlay = () => {
+    if (!overlayOpened) return;
+    searchOverlay.classList.remove(activeOverlayClass);
+    document.body.classList.remove(bodyNoScrollClass);
+    overlayOpened = false;
+};
 
 openSearchBtn.addEventListener('click', openOverlay);
 closeSearchBtn.addEventListener('click', closeOverlay);
+document.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 's': openOverlay(); break;
+        case 'Escape': closeOverlay(); break;
+    }
+})
