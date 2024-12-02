5e407c4ca24d33b12a15e30feffb61a5c04dc3d3
lapeko <lapeko88@gmail.com>
2024-12-02 19:23:48 +0100

    77. Working With Multiple Post Types

--- www/html/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ www/html/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -8,19 +8,45 @@
 
 	function university_search_results($data) {
 		$professors = new WP_Query(array(
-			'post_type' => 'professor',
+			'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
 			's' => sanitize_text_field($data['term']),
+			'posts_per_page' => $data['limit'] ?: 0,
+			'paged' => $data['page'] ?: 1,
 		));
 
-		$response = array();
+		$response = array(
+			'generalInfo' => array(),
+			'professors' => array(),
+			'programs' => array(),
+			'events' => array(),
+			'campuses' => array(),
+		);
 
 		while ($professors->have_posts()) {
 			$professors->the_post();
-			$response[] = array(
-				'title'     => get_the_title(),
-				'permalink' => get_the_permalink(),
-			);
+			$postType = get_post_type();
+
+			switch ($postType) {
+				case 'post':
+				case 'page':
+						array_push($response['generalInfo'], fillPostInfo()); break;
+				case 'professor':
+						array_push($response['professors'], fillPostInfo()); break;
+				case 'program':
+					array_push($response['programs'], fillPostInfo()); break;
+				case 'events':
+					array_push($response['events'], fillPostInfo()); break;
+				case 'campus':
+					array_push($response['campuses'], fillPostInfo()); break;
+			}
 		}
 
 		return $response;
+	}
+
+	function fillPostInfo() {
+		return array(
+			'title'     => get_the_title(),
+			'permalink' => get_the_permalink(),
+		);
 	}
