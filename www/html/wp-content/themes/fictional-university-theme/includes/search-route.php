753e53836a08d86b4f20d7c6a28d319c9425d835
lapeko <lapeko88@gmail.com>
2024-12-03 03:57:40 +0100

    83. Completing Our Search Overlay

--- www/html/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ www/html/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -7,7 +7,7 @@
 	}
 
 	function university_search_results($data): array {
-		$professors = new WP_Query(array(
+		$items = new WP_Query(array(
 			'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
 			's' => sanitize_text_field($data['term']),
 			'posts_per_page' => $data['limit'] ?: 0,
@@ -22,8 +22,8 @@
 			'campuses' => array(),
 		);
 
-		while ($professors->have_posts()) {
-			$professors->the_post();
+		while ($items->have_posts()) {
+			$items->the_post();
 			$postType = get_post_type();
 
 			switch ($postType) {
@@ -56,34 +56,70 @@
 				$item['content'] = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
 				break;
 			case 'programs':
-				$array['professors'] = array_merge($array['professors'], getRelatedProfessors(get_the_ID()));
+				$relatedData = getProfessorRelatedData(get_the_ID());
+				$array['professors'] = array_merge($array['professors'], $relatedData['professors']);
+				$array['events'] = array_merge($array['events'], $relatedData['events']);
+				$array['campuses'] = array_merge($array['campuses'], $relatedData['campuses']);
 				break;
 		}
 
 		$array[$key][] = $item;
 		$array['professors'] = array_unique($array['professors'], SORT_REGULAR);
+		$array['events'] = array_unique($array['events'], SORT_REGULAR);
+		$array['campuses'] = array_unique($array['campuses'], SORT_REGULAR);
 
 		return $array;
 	}
 
-	function getRelatedProfessors($professorId): array {
+	function getProfessorRelatedData($programId): array {
+		$wholeCampuses = get_field('related_campuses');
 		$professorsQuery = new WP_Query(array(
-			'post_type' => 'professor',
+			'post_type' => array('professor', 'event'),
+			'relation' => 'OR',
 			'meta_query' => array(
 				array(
 					'key' => 'related_programs',
 					'compare' => 'LIKE',
-					'value' => $professorId,
+					'value' => $programId,
 				)
 			),
 		));
+
 		$professors = array();
+		$events = array();
+
 		while ($professorsQuery->have_posts()): $professorsQuery->the_post();
-			$professors[] = array(
-				'title'     => get_the_title(),
-				'permalink' => get_the_permalink(),
-				'thumbnail' => get_the_post_thumbnail_url( 0, 'professorLandscape' ),
-			);
+			if (get_post_type() === 'professor') {
+				$professors[] = array(
+					'title'     => get_the_title(),
+					'permalink' => get_the_permalink(),
+					'thumbnail' => get_the_post_thumbnail_url( 0, 'professorLandscape' ),
+				);
+			}
+			if (get_post_type() === 'event') {
+				$eventDate = new DateTime( get_field( 'event_date' ) );
+				$events[]  = array(
+					'title'     => get_the_title(),
+					'permalink' => get_the_permalink(),
+					'month'     => $eventDate->format( 'M' ),
+					'day'       => $eventDate->format( 'd' ),
+					'content'   => has_excerpt() ? get_the_excerpt() : wp_trim_words( get_the_content(), 18 ),
+				);
+			}
 		endwhile;
-		return $professors;
+
+		$campuses = array();
+
+		foreach ($wholeCampuses as $campus) {
+			$campuses[] = array(
+				'title' => get_the_title($campus),
+				'permalink' => get_the_permalink($campus),
+			);
+		}
+
+		return array(
+			'professors' => $professors,
+			'events' => $events,
+			'campuses' => $campuses,
+		);
 	}
